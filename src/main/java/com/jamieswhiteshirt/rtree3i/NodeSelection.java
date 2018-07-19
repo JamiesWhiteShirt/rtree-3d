804f4eb67540071f7608cddd0c374b9faa6359a2
Erlend Åmdal <erlend@aamdal.com>
2018-07-19 21:16:29 +0200

    Separate filtered and unfiltered node selection

--- src/main/java/com/jamieswhiteshirt/rtree3i/NodeSelection.java
+++ src/main/java/com/jamieswhiteshirt/rtree3i/NodeSelection.java
@@ -3,106 +3,70 @@ package com.jamieswhiteshirt.rtree3i;
 import java.util.function.*;
 import java.util.stream.Collector;
 
-public class NodeSelection<K, V, T> implements Selection<T> {
-    private static final Predicate<Object> ALWAYS_TRUE = o -> true;
-
+final class NodeSelection<K, V, T> implements Selection<T> {
     public static <K, V, T> Selection<T> create(Node<K, V> root, Predicate<? super Box> boxPredicate,
                                                 Function<Entry<K, V>, T> entryMapper) {
-        return new NodeSelection<>(root, boxPredicate, ALWAYS_TRUE, entryMapper);
+        return new NodeSelection<>(root, boxPredicate, entryMapper);
     }
 
     private final Node<K, V> node;
     private final Predicate<? super Box> boxPredicate;
-    private final Predicate<? super T> filter;
     private final Function<Entry<K, V>, T> entryValueMapper;
 
-    private NodeSelection(Node<K, V> node, Predicate<? super Box> boxPredicate, Predicate<? super T> filter,
-                          Function<Entry<K, V>, T> entryMappper) {
+    private NodeSelection(Node<K, V> node, Predicate<? super Box> boxPredicate, Function<Entry<K, V>, T> entryMappper) {
         this.node = node;
         this.boxPredicate = boxPredicate;
-        this.filter = filter;
         this.entryValueMapper = entryMappper;
     }
 
     @Override
     public Selection<T> filter(Predicate<? super T> predicate) {
-        if (this.filter == ALWAYS_TRUE) {
-            return new NodeSelection<>(node, boxPredicate, predicate, entryValueMapper);
-        } else {
-            Predicate<? super T> filter = this.filter;
-            return new NodeSelection<>(node, boxPredicate, t -> filter.test(t) && predicate.test(t), entryValueMapper);
-        }
+        return new FilteredNodeSelection<>(node, boxPredicate, predicate, entryValueMapper);
     }
 
     @Override
     public void forEach(Consumer<? super T> action) {
-        node.forEach(boxPredicate, entry -> {
-            T value = entryValueMapper.apply(entry);
-            if (filter.test(value)) {
-                action.accept(entryValueMapper.apply(entry));
-            }
-        });
+        node.forEach(boxPredicate, entry -> action.accept(entryValueMapper.apply(entry)));
     }
 
     @Override
     public boolean anyMatch(Predicate<? super T> predicate) {
-        return node.anyMatch(boxPredicate, entry -> {
-            T value = entryValueMapper.apply(entry);
-            return filter.test(value) && predicate.test(value);
-        });
+        return node.anyMatch(boxPredicate, entry -> predicate.test(entryValueMapper.apply(entry)));
     }
 
     @Override
     public boolean allMatch(Predicate<? super T> predicate) {
-        return node.allMatch(boxPredicate, entry -> {
-            T value = entryValueMapper.apply(entry);
-            return !filter.test(value) || predicate.test(value);
-        });
+        return node.allMatch(boxPredicate, entry -> predicate.test(entryValueMapper.apply(entry)));
     }
 
     @Override
     public boolean noneMatch(Predicate<? super T> predicate) {
-        return !node.anyMatch(boxPredicate, entry -> {
-            T value = entryValueMapper.apply(entry);
-            return !filter.test(value) || predicate.test(value);
-        });
+        return !node.anyMatch(boxPredicate, entry -> predicate.test(entryValueMapper.apply(entry)));
     }
 
     @Override
     public T reduce(T identity, BinaryOperator<T> accumulator) {
-        return node.reduce(boxPredicate, identity, (acc, entry) -> {
-            T value = entryValueMapper.apply(entry);
-            return filter.test(value) ? accumulator.apply(acc, value) : acc;
-        });
+        return node.reduce(boxPredicate, identity, (acc, entry) -> accumulator.apply(acc,
+            entryValueMapper.apply(entry)));
     }
 
     @Override
     public <R> R collect(Supplier<R> supplier, BiConsumer<R, ? super T> accumulator, BiConsumer<R, R> combiner) {
         R container = supplier.get();
-        node.forEach(boxPredicate, entry -> {
-            T value = entryValueMapper.apply(entry);
-            if (filter.test(value)) {
-                accumulator.accept(container, value);
-            }
-        });
+        node.forEach(boxPredicate, entry -> accumulator.accept(container, entryValueMapper.apply(entry)));
         return container;
     }
 
     @Override
     public <R, A> R collect(Collector<? super T, A, R> collector) {
         A container = collector.supplier().get();
-        node.forEach(boxPredicate, entry -> {
-            T value = entryValueMapper.apply(entry);
-            if (filter.test(value)) {
-                collector.accumulator().accept(container, value);
-            }
-        });
+        node.forEach(boxPredicate, entry -> collector.accumulator().accept(container, entryValueMapper.apply(entry)));
         return collector.finisher().apply(container);
     }
 
     @Override
     public int count() {
-        return node.count(boxPredicate, entry -> filter.test(entryValueMapper.apply(entry)));
+        return node.count(boxPredicate, entry -> true);
     }
 
     @Override
@@ -112,6 +76,6 @@ public class NodeSelection<K, V, T> implements Selection<T> {
 
     @Override
     public boolean isNotEmpty() {
-        return node.anyMatch(boxPredicate, entry -> filter.test(entryValueMapper.apply(entry)));
+        return node.anyMatch(boxPredicate, entry -> true);
     }
 }
